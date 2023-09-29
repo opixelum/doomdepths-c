248d1d77aa3726a6a395da297b60fbbc1aeb296a
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-09-29 17:34:19 +0200

    Add main menu (#10)
    * Declare all menus related functions
    
    * Define all menus related functions
    
    * Add main loop & call main menu
    
    * Add TODO comments on options

--- include/menus.h
+++ include/menus.h
@@ -1,4 +1,63 @@
 #ifndef MENUS_H
 #define MENUS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Cross-platform function to clear console screen.
+ *
+ * Prints a regex where:
+ * - "\e[1;1H" moves cursor to top left corner of terminal screen;
+ * - "\e[2J" replaces all characters of the terminal screen with spaces.
+ */
+void clear_screen();
+
+/**
+ * @brief Cross-platform functino to clear a given number of console lines.
+ *
+ * Prints a regex where:
+ * - "\x1b[1F" moves cursor to beginning of previous line;
+ * - "\x1b[2K" replaces all characters of this line with spaces.
+ *
+ * @param number_of_lines Number of lines to clear
+ */
+void clear_lines(unsigned int number_of_lines);
+
+/**
+ * @brief Clears stdin buffer (useful before getting user input)
+ *
+ * When calling getchar(), it clears a character from stdin buffer.
+ * While it's not a newline character, or EOF, it keeps clearing characters.
+ */
+void clear_stdin();
+
+/**
+ * @brief Waits for user to press [ENTER] key.
+ *
+ * Uses getchar() in clearStdin() to wait for [ENTER] key.
+ * Then, it clears the stdin buffer (in case user pressed more than one key).
+ */
+void wait_for_enter();
+
+/**
+ * @brief Get the user input from stdin
+ *
+ * @return Pointer to the string or character containing user input
+ */
+char *get_user_input();
+
+/**
+ * @brief Prints main menu.
+ *
+ * 3 options:
+ * 1. New Game;
+ * 2. Continue Game;
+ * 3. Quit.
+ *
+ * @param is_running Pointer to the variable that controls the main loop
+ */
+void main_menu(unsigned char *is_running);
+
 #endif // MENUS_H
--- src/menus.c
+++ src/menus.c
@@ -1,1 +1,109 @@
 #include "menus.h"
+
+void clear_screen()
+{
+    printf("\e[1;1H\e[2J");
+}
+
+void clear_lines(unsigned int number_of_lines)
+{
+    for (int i = 0; i < number_of_lines; ++i) printf("\x1b[1F\x1b[2K");
+}
+
+void clear_stdin()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+void wait_for_enter()
+{
+    printf("\nPress [ENTER] to continue...");
+    clear_stdin();
+}
+
+char *get_user_input()
+{
+    // Allocate memory for the string with the maximum size for 255 characters
+    char *input = malloc(256 * sizeof input);
+    if (!input)
+    {
+        fprintf(stderr, "ERROR: menus.c: get_user_input: `input` malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get user input from stdin buffer
+    fgets(input, 256, stdin);
+
+    // Remove trailing newline & retrun carriage characters
+    input[strcspn(input, "\n\r")] = '\0';
+
+    // Check if input is a string or a character
+    unsigned char input_length = strlen(input);
+
+    // If it's a character, remove the null terminator by reallocating memory
+    if (input_length == 1) input = realloc(input, sizeof input);
+
+    // If it's a string, reallocate memory to the actual size of the string
+    // & add 1 for the null terminator
+    else input = realloc(input, input_length + 1 * sizeof input);
+
+    // Check if memory reallocation failed
+    if (!input)
+    {
+        fprintf(stderr, "ERROR: menus.c: get_user_input: `input` realloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return input;
+}
+
+void main_menu(unsigned char *is_running)
+{
+    // First loop for repeating the menu if user enters an invalid choice
+    do
+    {
+        char *input = NULL;
+        clear_lines(10); // Main menu is at most 10 lines long
+        printf("DoomdepthsC\n\n    1. New Game\n    2. Continue Game\n    3. Quit\n\n");
+
+        // Second loop for repeating the menu if user enters an empty string
+        do
+        {
+            if (input) // Re-print the last line if user entered an empty string
+            {
+                clear_lines(1);
+                free(input);
+            }
+            printf("Enter your choice (1, 2 or 3): ");
+            input = get_user_input();
+        }
+        while (!strlen(input)); // While length of input is 0
+
+        switch (input[0])
+        {
+            case '1':
+                // TODO: Implement New Game
+                printf("New Game\n");
+                break;
+
+            case '2':
+                // TODO: Implement Continue Game
+                printf("Continue Game\n");
+                break;
+
+            case '3':
+                clear_screen();
+                *is_running = 0;
+                free(input);
+                return;
+
+            default:
+                printf("Invalid choice!\n");
+                break;
+        }
+
+        free(input);
+        wait_for_enter();
+    } while (1);
+}
