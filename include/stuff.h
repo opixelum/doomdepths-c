ceaf22c51dd5bc158be6e60f9238544ae899f4f3
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-03 12:59:32 +0200

    Add stuff structs

--- include/stuff.h
+++ include/stuff.h
@@ -1,4 +1,29 @@
 #ifndef STUFF_H
 #define STUFF_H
 
+#define MAX_INVENTORY_SIZE 25
+
+typedef enum ItemType
+{
+    WEAPON,
+    ARMOR,
+    HEALTH_POTION,
+    MANA_POTION
+} ItemType;
+
+typedef struct Item
+{
+    ItemType type;
+    char *name;
+    char *description;
+    unsigned short value; // Dealing damage if weapon, reducing damage if armor, healing if health potion, etc.
+    unsigned short price; // In gold
+} Item;
+
+typedef struct Inventory
+{
+    Item *item;
+    struct Inventory *next;
+} Inventory;
+
 #endif // STUFF_H
