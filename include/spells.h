fdcdf4f6c5c8c113e258aa23538965bf23cc0d3d
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-03 12:58:24 +0200

    Add spells structs

--- include/spells.h
+++ include/spells.h
@@ -1,4 +1,17 @@
 #ifndef SPELLS_H
 #define SPELLS_H
 
+typedef struct Spell
+{
+    char *name;
+    char *description;
+    unsigned short value; // Dealing damage if attack spell, reducing damage if defense spell, etc.
+    unsigned short mana_cost;
+} Spell;
+
+typedef struct Spells {
+    Spell *spell;
+    struct Spells *next;
+} Spells;
+
 #endif // SPELLS_H
