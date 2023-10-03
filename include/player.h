b968bc86fda9efd1f63f1ee19040ccb4b86f6e23
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-03 12:59:44 +0200

    Add player stuff

--- include/player.h
+++ include/player.h
@@ -1,4 +1,21 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "spells.h"
+#include "stuff.h"
+
+typedef struct Player
+{
+    char *name;
+    unsigned short health;
+    unsigned short max_health;
+    unsigned short mana;
+    unsigned short max_mana;
+    unsigned long gold;
+    Item *weapon;
+    Item *armor;
+    Spells *spells; // Head of the linked list
+    Inventory *inventory; // Head of the linked list
+} Player;
+
 #endif // PLAYER_H
