d84b26bd5c0245f684079eea123c8d57f50f2244
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-05 16:31:45 +0200

    Add stuff code

--- include/stuff.h
+++ include/stuff.h
@@ -1,6 +1,9 @@
 #ifndef STUFF_H
 #define STUFF_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define MAX_INVENTORY_SIZE 25
 
 typedef enum ItemType
@@ -28,5 +31,57 @@ typedef struct Inventory
     struct Inventory *next;
 }
 Inventory;
+
+/**
+ * @brief Initialize a new item
+ * @param type The type of the item (WEAPON, ARMOR, HEALTH_POTION, MANA_POTION
+ * @param name Pointer to the name of the item
+ * @param description Pointer to the description of the item
+ * @param value The value of the item (see Item.value)
+ * @param price The price of the item in gold
+ * @return A pointer to the new item
+ * @warning The item must be freed with free_item() when it is no longer needed
+ */
+Item *create_item
+(
+    ItemType type,
+    char *name,
+    char *description,
+    unsigned short value,
+    unsigned short price
+);
+
+/**
+ * @brief Free an item from memory
+ * @param item A pointer to the item to be freed
+ */
+void free_item(Item *item);
+
+/**
+ * @brief Add an item to the end of an inventory
+ * @param head Pointer to a node of the inventory
+ * @param item Pointer to the item to be added
+ * @return Pointer to the given inventory node or the new inventory node if the given inventory node was NULL
+ */
+Inventory *add_item_to_inventory(Inventory *node, Item *item);
+
+/**
+ * @brief Remove an item from an inventory
+ * @param head Pointer to the head of the inventory
+ * @param item Pointer to the item to be removed
+ * @return Pointer to the removed item, or NULL if the item was not found
+ * @warning This function does not free the item from memory, it only removes it from the inventory
+ * @warning In case you're not passing the head of the inventory, the item to remove can be skipped if it is before the
+ * given inventory node
+ */
+Item *remove_item_from_inventory(Inventory *head, Item *item);
+
+/**
+ * @brief Free a linked list of items from memory
+ * @param head A pointer to the head of the linked list of items to be freed
+ * @warning Every item in the linked list will be freed from memory
+ * @warning In case you're not passing the head of the inventory, only the following nodes will be freed
+ */
+void free_inventory(Inventory *head);
 
 #endif // STUFF_H
--- src/stuff.c
+++ src/stuff.c
@@ -1,1 +1,96 @@
 #include "stuff.h"
+
+Item *create_item
+(
+    ItemType type,
+    char *name,
+    char *description,
+    unsigned short value,
+    unsigned short price
+) {
+    Item *item = malloc(sizeof *item);
+    if (!item)
+    {
+        fprintf(stderr, "ERROR: stuff.c: create_item: item: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    item->type = type;
+    item->name = name;
+    item->description = description;
+    item->value = value;
+    item->price = price;
+    return item;
+}
+
+void free_item(Item *item)
+{
+    if (!item) return;
+    free(item->name);
+    free(item->description);
+    free(item);
+}
+
+Inventory *add_item_to_inventory(Inventory *node, Item *item_to_add)
+{
+    Inventory *new_item_entry = malloc(sizeof *new_item_entry);
+    if (!new_item_entry)
+    {
+        fprintf(stderr, "ERROR: stuff.c: add_item_to_inventory: new_item_entry: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_item_entry->item = item_to_add;
+    new_item_entry->next = NULL;
+
+    // If the inventory is empty, the new item becomes the first item
+    if (!node) return new_item_entry;
+
+    // Otherwise, add the new item to the end of the inventory
+    Inventory *current = node;
+    while (current->next) current = current->next;
+    current->next = new_item_entry;
+
+    return node;
+}
+
+Item *remove_item_from_inventory(Inventory *head, Item *item_to_remove)
+{
+    Inventory *prev = NULL;
+    Inventory *current = head;
+
+    while (current)
+    {
+        if (current->item == item_to_remove)
+        {
+            // If the item to remove is at the head of the inventory.
+            if (!prev)
+            {
+                Item *removedItem = current->item;
+                free(current);
+                return removedItem;
+            }
+
+            // Item found in the middle or end of the inventory.
+            Item *removedItem = current->item;
+            prev->next = current->next;
+            free(current);
+            return removedItem;
+        }
+
+        prev = current;
+        current = current->next;
+    }
+
+    return NULL; // Item not found in the inventory.
+}
+
+void free_inventory(Inventory *head)
+{
+    while (head)
+    {
+        Inventory *next = head->next;
+        if (head->item) free_item(head->item);
+        free(head);
+        head = next;
+    }
+}
