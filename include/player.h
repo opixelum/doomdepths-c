81ceec35b310610d457e4e5f27f79065c6e622b1
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-05 17:06:06 +0200

    Add player code

--- include/player.h
+++ include/player.h
@@ -12,11 +12,25 @@ typedef struct Player
     unsigned short mana;
     unsigned short max_mana;
     unsigned long gold;
-    Item *weapon;
-    Item *armor;
-    Spells *spells; // Head of the linked list
+    Item *weapon; // Equipped weapon isn't in the inventory
+    Item *armor; // Equipped armor isn't in the inventory
+    Inventory *spells; // Head of the linked list
     Inventory *inventory; // Head of the linked list
 }
 Player;
+
+/**
+ * @brief Initialize a new player
+ * @param name Pointer to the name of the player
+ * @return Pointer to the new player
+ * @warning The player must be freed with free_player() when it is no longer needed
+ */
+Player *create_player(char *name);
+
+/**
+ * @brief Free a player from memory
+ * @param player Pointer to the player to be freed
+ */
+void free_player(Player *player);
 
 #endif // PLAYER_H
