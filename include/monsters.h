63201248d59a8ad139378c3fad88d79302966f61
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-06 17:10:03 +0200

    Add `generate_random_monster`

--- include/monsters.h
+++ include/monsters.h
@@ -1,4 +1,31 @@
 #ifndef MONSTERS_H
 #define MONSTERS_H
 
+#include <string.h>
+#include <time.h>
+
+#include "character.h"
+
+/**
+ * @brief Generate a monster with random stats.
+ *
+ * - 25% chance to be a Goblin;
+ * - 20% chance to be an Orc;
+ * - 20% chance to be a Troll;
+ * - 15% chance to be an Ogre;
+ * - 15% chance to be a Giant;
+ * - 5% chance to be a Dragon.
+ *
+ * Level is a random number between 1 and 100.
+ * XP is a random number between 0 and 1000.
+ * Gold is a random number between 0 and 1000.
+ * Health is calculated with the following formula:
+ * health = base_health + base_health * (level / 100)
+ * Mana is equal to health.
+ *
+ * @return Pointer to the new monster
+ * @warning The monster must be freed with free_character() when it is no longer needed
+ */
+Character *generate_random_monster();
+
 #endif // MONSTERS_H
--- src/monsters.c
+++ src/monsters.c
@@ -1,1 +1,92 @@
 #include "monsters.h"
+
+Character *generate_random_monster()
+{
+    srand(time(NULL));
+
+    char *name = calloc(7, sizeof *name);
+    unsigned char level = 0;
+    unsigned int xp = 0;
+    unsigned int xp_to_next_level = 0;
+    unsigned short health = 0;
+    unsigned short max_health = 0;
+    unsigned short mana = 0;
+    unsigned short max_mana = 0;
+    unsigned long gold = 0;
+
+    // Assign monster stats based on monster type
+    unsigned char random_monster_type = rand() % 100;
+
+    level = rand() % 100 + 1;
+    xp = rand() % 1000;
+    xp_to_next_level = 0;
+    gold = rand() % 1000;
+
+    if (random_monster_type < 25) // 25% chance
+    {
+        strcpy(name, "Goblin");
+        health = 50.0 + 50.0 * (level / 100.0);
+        max_health = health;
+        mana = health;
+        max_mana = health;
+    }
+    else if (random_monster_type < 45) // 20% chance
+    {
+        strcpy(name, "Orc");
+        health = 80.0 + 80.0 * (level / 100.0);
+        max_health = health;
+        mana = health;
+        max_mana = health;
+    }
+    else if (random_monster_type < 65) // 20% chance
+    {
+        strcpy(name, "Troll");
+        health = 90.0 + 90.0 * (level / 100.0);
+        max_health = health;
+        mana = health;
+        max_mana = health;
+    }
+    else if (random_monster_type < 80) // 15% chance
+    {
+        strcpy(name, "Ogre");
+        health = 120.0 + 120.0 * (level / 100.0);
+        max_health = health;
+        mana = health;
+        max_mana = health;
+    }
+    else if (random_monster_type < 95) // 15% chance
+    {
+        strcpy(name, "Giant");
+        health = 130.0 + 130.0 * (level / 100.0);
+        max_health = health;
+        mana = health;
+        max_mana = health;
+    }
+    else // %5 chance
+    {
+        strcpy(name, "Dragon");
+        health = 200.0 + 200.0 * (level / 100.0);
+        max_health = health;
+        mana = health;
+        max_mana = health;
+    }
+    name = realloc(name, (strlen(name) + 1) * sizeof *name);
+
+    return create_character
+    (
+        name,
+        level,
+        xp,
+        xp_to_next_level,
+        health,
+        max_health,
+        mana,
+        max_mana,
+        gold,
+        NULL,
+        NULL,
+        // TODO: Give random items to monsters for player to loot
+        NULL,
+        NULL
+    );
+}
