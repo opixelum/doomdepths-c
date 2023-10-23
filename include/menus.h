5d062ce8579e602a0aea96761385ea9ba1e4b129
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-23 15:49:50 +0200

    Display users stats (#31)
    * Add `color_printf()`
    
    * Add `print_stat_bar()`
    
    * Add `print_character_stats()`

--- include/menus.h
+++ include/menus.h
@@ -1,6 +1,7 @@
 #ifndef MENUS_H
 #define MENUS_H
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -85,5 +86,35 @@ Character *monster_selection_menu(Monsters *head);
  * @return 1 if weapon, 2 if spell, 0 on error
  */
 unsigned char attack_selection_menu();
+
+/**
+ * @brief `printf()`, but in color
+ * @param hexcolor Color to print in hexadecimal format
+ * @param format Format string
+ * @param ... Arguments to format string
+ */
+void color_printf(unsigned int hexcolor, const char *format, ...);
+
+/**
+ * @brief Print a bar with current and max values in percentage
+ * @param label String to print before the bar
+ * @param current Current value
+ * @param max Max value
+ * @param color Color of the bar in hexadecimal, or -1 for adaptive color
+ * (red if < 10%, yellow if < 30%, green otherwise)
+ */
+void print_stat_bar
+(
+    const char *label,
+    unsigned int current,
+    unsigned int max,
+    int color
+);
+
+/**
+ * @brief Prints health & mana bars & their gold
+ * @param character Pointer to the character
+ */
+void print_character_stats(Character *character);
 
 #endif // MENUS_H
