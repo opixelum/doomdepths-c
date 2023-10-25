a35d6e1852d57f1ff70ff66e445962a8aff45793
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-25 18:16:38 +0200

    Define `number_of_attack_spells()`

--- src/spells.c
+++ src/spells.c
@@ -1,1 +1,18 @@
 #include "spells.h"
+
+unsigned char number_of_attack_spell(Character *character)
+{
+    if (!character) return 0;
+    if (!character->spells) return 0;
+
+    Inventory *node = character->spells;
+    unsigned char number_of_attack_spell = 0;
+
+    while (node)
+    {
+        if (node->item->type == ATTACK_SPELL) number_of_attack_spell++;
+        node = node->next;
+    }
+
+    return number_of_attack_spell;
+}
