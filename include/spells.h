4fa6e88df90a7988b12d12775e3b86cdaeb7a61e
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-25 18:16:15 +0200

    Declare `number_of_attack_spells()`

--- include/spells.h
+++ include/spells.h
@@ -1,4 +1,14 @@
 #ifndef SPELLS_H
 #define SPELLS_H
 
+#include "character.h"
+
+/**
+ * @brief Useful function for knowing if we need to display the spell menu or
+ * not when the player is fighting.
+ * @param character A point to the character.
+ * @return The number of attack spells the character has.
+ */
+unsigned char number_of_attack_spells(Character *character);
+
 #endif // SPELLS_H
