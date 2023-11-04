538877c7dc6d1360a913d2ee20799e0982fc1cab
SEILLIER Louis <43609249+Loulou2Mc@users.noreply.github.com>
2023-11-04 21:26:55 +0100

    Add SQLite (#67)
    * adding first save functions
    
    * addind xp and gold to player
    
    * Fix project name in `CMakeLists.txt`
    
    * Fix wrong include
    
    * fix include
    
    * fix Player to Character
    
    ---------
    
    Co-authored-by: Opixelum <48217573+opixelum@users.noreply.github.com>

--- include/save.h
+++ include/save.h
@@ -1,4 +1,12 @@
 #ifndef SAVE_H
 #define SAVE_H
 
+#include "character.h"
+#include <stdio.h>
+#include <sqlite3.h>
+#include "stuff.h"
+#include <string.h>
+void saveGame(Character player);
+Character continueGame();
+
 #endif // SAVE_H
