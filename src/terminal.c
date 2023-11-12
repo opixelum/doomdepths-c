940f9bfdc09cef219aa8cd5da04debc55371bc95
opixelum <48217573+opixelum@users.noreply.github.com>
2023-11-12 21:53:02 +0100

    Define `wait_milliseconds()`

--- src/terminal.c
+++ src/terminal.c
@@ -32,3 +32,11 @@ void press_enter_to_continue(void)
     while (getchar_no_enter() != '\n') {}
 }
 
+void wait_milliseconds(int milliseconds)
+{
+#ifdef _WIN32
+Sleep(milliseconds);
+#else
+usleep(milliseconds * 1000);  // Convert to microseconds
+#endif
+}
